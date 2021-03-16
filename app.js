9011b6fa36f2769413b070fa56fe1500db4e6a58
Rajabov Bobokhon <rboboxon6055@gmail.com>
2021-03-16 14:08:11 +0900

    update V3

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const submit = document.getElementById("submitBook");
-let myLibrary = [];
 
 //Book constructor
-function Book(title, author, number) {
+function Book(title, author, number, read) {
   this.title = title;
   this.author = author;
   this.number = number;
+  this.read = read;
 }
 
 submit.addEventListener("click", addBook);
@@ -15,10 +15,28 @@ function addBook(e) {
   const titleName = document.getElementById("titleBook").value;
   const authorName = document.getElementById("author").value;
   const pageNumber = document.getElementById("pageNumber").value;
+  const checkBox = document.getElementById("checkbox").checked;
 
-  let book = new Book(titleName, authorName, pageNumber);
+  let book = new Book(titleName, authorName, pageNumber, checkBox);
+  if (
+    titleName === "" ||
+    titleName.trim() === "" ||
+    authorName === "" ||
+    authorName.trim() === "" ||
+    pageNumber === ""
+  ) {
+    alert("Please write all information!!");
+  } else {
+    getBooks(book);
+    showMessage("Book Added", "success");
+    clear();
+  }
+}
 
-  getBooks(book);
+function clear() {
+  document.getElementById("titleBook").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("pageNumber").value = "";
 }
 
 function getBooks(book) {
@@ -26,14 +44,52 @@ function getBooks(book) {
 
   //create Elemnt
   const row = document.createElement("tr");
-
+  // row.className = "delete";
   //insert cols
   row.innerHTML = `
   <td>${book.title}</td>
   <td>${book.author}</td>
   <td>${book.number}</td>
-  <td> <a href = "#" class = ""delete>X</a> </td>`;
-  console.log(book.number);
-
+  <td >${book.read ? "Read" : "Not Read"}</td>
+  <td> <a href = "#" className="delete">X</a> </td>`;
   bookList.appendChild(row);
+  // console.log(bookList);
 }
+
+//show the message alert in DOM
+function showMessage(message, classTitle) {
+  const div = document.createElement("div");
+  //Add Class
+  div.className = `alert ${classTitle}`;
+  console.log(div);
+  //Add text
+  div.appendChild(document.createTextNode(message));
+  //get parent
+  const container = document.querySelector(".container");
+  //get form
+  const form = document.querySelector("#book-form");
+
+  //inser alert before the from
+  container.insertBefore(div, form);
+
+  //set time and remove alert
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 3000);
+}
+
+//remove from the list
+function removeItem(target) {
+  if ((target.className = "delete")) {
+    target.parentElement.parentElement.remove();
+  }
+}
+
+//addevent Listener to remove button
+const bookList = document
+  .getElementById("bookList")
+  .addEventListener("click", function (e) {
+    removeItem(e.target);
+    showMessage("Book Removed", "error");
+    e.preventDefault();
+  });
